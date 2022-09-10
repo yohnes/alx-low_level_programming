9adbcf11cc65584d9d033e24371fa17458d450e5
Yohanes <yohanesabate@yahoo.com>
2022-09-10 12:50:39 -0700

    task 4 uodate

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,24 +4,21 @@
 
 /**
 
-* main - print the string in the put function
+ * main - Entry point
 
-*
+ *
 
-* Description: using the main function
+ * Return: Always 0 (Success)
 
-* this program prints "Programming is like building a multilingual puzzle
-
-* Return: 0
-
-*/
+ */
 
 int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 
 }
+
