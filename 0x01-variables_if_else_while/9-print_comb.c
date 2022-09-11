ebd95eea36a43790511d4f18b59adcb653436bf9
Yohanes <yohanesabate@yahoo.com>
2022-09-11 01:41:08 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,17 @@
 */
 int main(void)
 {
-	int c;
-	
-	for (c = 48; c <= 57; c++)
-	{
-		putchar(c);
-		if (c != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+int c;
+
+for (c = 48; c <= 57; c++)
+{
+putchar(c);
+if (c != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
