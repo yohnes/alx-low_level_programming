058da5819365731c8d7dcb6990f1ae8cb366beb1
Yohanes <yohanesabate@yahoo.com>
2022-09-11 12:12:28 -0700

    TASK10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,15 +6,17 @@
 */
 int main(void)
 {
-int c = 0;
-int f_d;
-int l_d;
+      int c = 0;
+      int f_d;
+      int l_d;
 
 while (c <= 99)
 {
-f_d = (c / 10 + '0');
-l_d = (c % 10 + '0');
-f (f_d < l_d)
+
+        f_d = (c / 10 + '0');
+        l_d = (c % 10 + '0');
+
+        if (f_d < l_d)
 {
 putchar(f_d);
 putchar(l_d);
