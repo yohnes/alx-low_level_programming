7cc257e6df67527ffb43e9787e91516acc2dde22
Yohanes <yohanesabate@yahoo.com>
2022-10-27 23:29:06 -0700

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,11 @@
  */
 int main(void)
 {
-	char abe[] = "_putchar";
-
-	int c;
-
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(abe[c]);
-	}
-	_putchar('\n');
-	return (0);
+char abe[] = "_putchar";
+int c;
+for (c = 0; c < 8; c++)
+{
+_putchar(abe[c]);
+}
+_putchar('\n');
+return (0);
