38c757e358c2cf1a574294c8c12e090918eac8b2
Yohanes <yohanesabate@yahoo.com>
2022-10-27 23:20:17 -0700

    Nested Loop Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,17 @@
 #include "main.h"
-
 /**
- * main - prints _putchar
- *
- * Description: prints _putchar
- *
- * Return: 0 (Success)
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
  */
-
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
+	char abe[] = "_putchar";
 
-	for (i = 0; i < 8; i++)
+	int c;
+
+	for (c = 0; c < 8; c++)
 	{
-		_putchar(text[i]);
+		_putchar(abe[c]);
 	}
 	_putchar('\n');
-
 	return (0);
