53eb85f4cfa52476c9cebbb1c9c42b3722b19a18
Yohanes <yohanesabate@yahoo.com>
2022-12-19 05:24:34 -0800

    dubg task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 include "main.h"
 /**
-* _strlen - returns the length of a string
-* @s: string
-* Return: length
+*_strlen - returns the length of a string
+*@s: string
+*Return: length
 */
 int _strlen(char *s)
 {
